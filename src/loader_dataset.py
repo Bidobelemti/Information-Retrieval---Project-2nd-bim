ae521e093dbd7403d31b29f4278d93bc79bebe8b
Bidobelemti <mauriceuve3221@gmail.com>
2026-01-31 19:29:22 -0500

    Corregi bugs en load_names y get_caption_by_image_name

--- src/loader_dataset.py
+++ src/loader_dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-from preprocessing import clean_text
+from src.preprocessing import clean_text
 
 
 def load_names(csv_paths: list[str], img_dir: str)->pd.DataFrame:
@@ -22,15 +22,17 @@ def load_names(csv_paths: list[str], img_dir: str)->pd.DataFrame:
     df_name_img = pd.concat([pd.read_csv(p) for p in csv_paths],ignore_index=True)
     df_name_img = df_name_img[['name', 'imageURLs']].drop_duplicates(subset = 'name').dropna(subset = 'imageURLs')
     df_name_img['name'] = df_name_img['name'].apply(clean_text)
-    for name in df_name_img['name']:
-        for img_file in os.listdir(img_dir):
-            if not img_file.endswith('.jpg'):
-                continue
-            if name in img_file:
-                rows.append({
-                    'image_path': os.path.join(img_dir, img_file).replace('\\', '/'),
-                    'caption' : name
-                })
+    for img_file in os.listdir(img_dir):
+        if not img_file.endswith('.jpg'):
+            continue
+
+        img_stem = img_file.rsplit("_", 1)[0]
+
+        if img_stem in df_name_img['name'].values:
+            rows.append({
+                'image_path': os.path.join(img_dir, img_file).replace('\\', '/'),
+                'caption': img_stem
+            })
 
     return pd.DataFrame(rows)
 
@@ -49,6 +51,6 @@ def get_caption_by_image_name(img_name: str, df: pd.DataFrame)->str:
             El nombre del archivo que estamos buscando
     '''
     try:
-        return df.loc[img_name in df['image_path']]['caption'].values[0]
+        return df.loc[df['image_path'].str.contains(img_name), 'caption'].values[0]
     except:
         return 'No se ha encontrado.'
