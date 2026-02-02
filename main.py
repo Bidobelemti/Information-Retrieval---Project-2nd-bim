d845510657b4ec7350167dd0a26424daff6fa3ad
Bidobelemti <mauriceuve3221@gmail.com>
2026-02-02 16:53:39 -0500

    Secuencia de pasos hasta construcción de indice para consultas

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import os
 import numpy as np
+import pandas as pd
 
 from dotenv import load_dotenv
+
 from src.loader_dataset import load_names, get_caption_by_image_name
 from src.embeddings import generate_text_embeddings, generate_image_embeddings, combine_img_embeddings_text_embeddings
 from src.vector import build_vector_index, search_similar_vectors, save_index, load_index
-from src.search import search_unique_products
+from src.preprocessing import merge_captions_by_image, preprocess_documents
 
 load_dotenv()
 api_key = os.getenv('GEMINI_API_KEY')
+TOP_K = 3
 
-paths = ['data/Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv','data/Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products_May19.csv','data/1429_1.csv' ]
-img_dir = 'data/img'
+paths = ['../data/Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv','../data/Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products_May19.csv','../data/1429_1.csv' ]
+img_dir = '../data/img'
 
-df = load_names(paths, img_dir)
+df_raw = load_names(paths, img_dir)
+df = merge_captions_by_image(df_raw, image_col='image_path', caption_col='caption')
 
-embedding = generate_text_embeddings(df['caption'].to_list(), device = 'cuda')
-print(type(embedding))
+# Obteniendo los embeddings del corpus text
+df_processed = preprocess_documents(df['combined_caption'].tolist())
+df['prep_doc'] = df_processed['prep_doc']
+text_embeddings = generate_text_embeddings(df['prep_doc'].tolist(), device='cuda')
+df['text_embedding'] = [vec for vec in text_embeddings]
 
-embedding_img, paths = generate_image_embeddings('data/img/', device='cuda')
-print(type(embedding_img))
+# Obteniendo los embeddings del corpus imagen
+img_embeddings, img_paths = generate_image_embeddings(img_dir, device = 'cuda')
+df_img = pd.DataFrame({
+    'image_path' : [path for path in img_paths],
+    'img_embedding' : list(img_embeddings)
+})
+df = df.merge(df_img, on='image_path', how = 'left')
 
-embedding_for_search = np.array(combine_img_embeddings_text_embeddings(embedding, embedding_img))
+# Obteniendo indice generado a partir de texto e imagenes en FAISS
+combined_emb = combine_img_embeddings_text_embeddings(np.array(df['text_embedding']), np.array(df['img_embedding']))
 
-index = build_vector_index(embedding_for_search)
-query = str(input('Ingresa una consulta: '))
-df_test = search_unique_products(query, index, df)
-
-print(f"Top {len(df_test)} resultados únicos encontrados:")
-for idx, row in df_test.iterrows():
-    print(f"- Producto: {row['caption']}")
-    print(f"  Categoría: {row['categories']}")
-    print(f"  Distancia: {row['distance']:.4f}")
-    print("-" * 30)
+index_faiss = build_vector_index(np.array(combined_emb))
+save_index(index_faiss)
