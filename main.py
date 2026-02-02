8e3450ac4e99155787b5ae9e385c92165af53d5a
Bidobelemti <mauriceuve3221@gmail.com>
2026-02-01 19:23:30 -0500

    Implmentación de funciones en MAIN

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-from src.loader_dataset import load_dataset
-path = 'data/Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products_May19.csv'
-df = load_dataset(path)
-print(len(df))
+import os
+import numpy as np
+
+from dotenv import load_dotenv
+from src.loader_dataset import load_names, get_caption_by_image_name
+from src.embeddings import generate_text_embeddings, generate_image_embeddings, combine_img_embeddings_text_embeddings
+from src.vector import build_vector_index, search_similar_vectors, save_index, load_index
+from src.search import search_unique_products
+
+load_dotenv()
+api_key = os.getenv('GEMINI_API_KEY')
+
+paths = ['data/Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products.csv','data/Datafiniti_Amazon_Consumer_Reviews_of_Amazon_Products_May19.csv','data/1429_1.csv' ]
+img_dir = 'data/img'
+
+df = load_names(paths, img_dir)
+
+embedding = generate_text_embeddings(df['caption'].to_list(), device = 'cuda')
+print(type(embedding))
+
+embedding_img, paths = generate_image_embeddings('data/img/', device='cuda')
+print(type(embedding_img))
+
+embedding_for_search = np.array(combine_img_embeddings_text_embeddings(embedding, embedding_img))
+
+index = build_vector_index(embedding_for_search)
+query = str(input('Ingresa una consulta: '))
+df_test = search_unique_products(query, index, df)
+
+print(f"Top {len(df_test)} resultados únicos encontrados:")
+for idx, row in df_test.iterrows():
+    print(f"- Producto: {row['caption']}")
+    print(f"  Categoría: {row['categories']}")
+    print(f"  Distancia: {row['distance']:.4f}")
+    print("-" * 30)
--- src/search.py
+++ src/search.py
@@ -2,3 +2,25 @@ import numpy as np
 from src.embeddings import generate_image_embeddings, generate_text_embeddings
 from src.vector import search_similar_vectors
 
+def search_unique_products(query_text, index, metadata_df, n=3):
+    """
+    Busca n productos únicos.
+    """
+    # 1. Generar el embedding de la consulta
+    q_emb = generate_text_embeddings(query_text) #
+    
+    # 2. Oversampling: Pedimos más de n para compensar duplicados (ej: n * 5)
+    # k_search debe ser suficiente para encontrar n elementos únicos
+    k_search = n * 5 
+    D, I = search_similar_vectors(q_emb, index, K=k_search) #
+    
+    # 3. Mapear índices de FAISS al DataFrame de metadatos
+    # I[0] contiene los índices de las filas más cercanas
+    results_df = metadata_df.iloc[I[0]].copy()
+    results_df['distance'] = D[0]
+    
+    # 4. Filtrar duplicados por 'caption' (o 'name') y tomar los primeros n
+    # keep='first' asegura que mantengas el que tiene la menor distancia/mejor score
+    final_results = results_df.drop_duplicates(subset=['caption'], keep='first').head(n)
+    
+    return final_results
